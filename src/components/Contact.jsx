ca5003972d197915f008d838a0249ed1feb2d5a3
ReetamBG <r33t4m@gmail.com>
2025-06-12 23:33:39 +0530

    Finally friggin completed it was a headache at the end ufff

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -43,7 +43,7 @@ const Contact = () => {
             className="special-font !md:text-[6.2rem] w-full font-zentry !text-5xl !font-black !leading-[.9]"
           />
 
-          <Button text="contact us" containerClass="mt-10 cursor-pointer" />
+          <Button title="contact us" containerClass="mt-10 cursor-pointer text-black" />
         </div>
       </div>
     </div>
