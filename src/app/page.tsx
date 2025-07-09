ffd1fef90e22a9a5a8e66c633d8d65947ae5cf12
ReetamBG <r33t4m@gmail.com>
2025-07-10 02:35:47 +0530

    Landing page done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import ExperienceSection from "@/app/_components/ExperienceSection";
 import HeroSection from "@/app/_components/HeroSection";
 import ProjectsSection from "@/app/_components/ProjectsSection";
 import SkillsSection from "@/app/_components/SkillsSection";
-// import SplashScreen from "@/components/utilityComponents/SpashScreen";
+import SplashScreen from "@/components/utilityComponents/SpashScreen";
 
 
 export default function Home() {
   return (
     <>
-        {/* <SplashScreen  /> */}
+        <SplashScreen  />
         <main className="relative w-full h-auto flex justify-center overflow-x-hidden">
           <div className="mt-32 mb-24 mx-5 sm:mx-10 max-w-3xl">
             <HeroSection />
